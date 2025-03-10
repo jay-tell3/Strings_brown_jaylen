ab304d0d1b95872e2500b8ad1ce7542a42f3a798
jay-tell3 <jaybroww@gmail.com>
2025-03-10 13:58:58 -0500

    bruh

--- Program.cs
+++ Program.cs
@@ -3,8 +3,23 @@
     internal class Program
     {
         static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
+        {   //ask for letter and makes a varible that is the same as the answer.
+            Console.WriteLine("please put one letter");  
+            char myChar = Console.ReadKey().KeyChar;
+            // check if one letter is enter
+            bool isLetter = char.IsLetter(myChar);
+            // write tells the user if it is a letter
+            Console.WriteLine($"\n{myChar} is a letter: {isLetter}.");
+            // ask for part of the bee movies script and makes the answer a varible
+            Console.WriteLine("please put part of the bee movie script here:");
+            string sentence = Console.ReadLine();
+            // ask the user to enter the word bee and save what they say
+            Console.WriteLine("enter thee word bee");
+            string word = Console.ReadLine();
+            //checks if the word is in the sentence
+            bool isIn = sentence.Contains(word);
+            //tells the user if the word was in the sentence
+            Console.WriteLine($"{word} exists in {sentence}, {isIn}!");
         }
     }
 }
